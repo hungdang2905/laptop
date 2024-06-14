805aad7245268b2af8091b8f6dc46cea8386345a
hungdang2905 <20130271@st.hcmuaf.edu.vn>
2024-06-14 21:15:57 +0700

    1

---
+++ src/test/java/vn/id/quanghuydevfs/drcomputer/DrComputerApplicationTests.java
@@ -0,0 +1,18 @@
+package vn.id.quanghuydevfs.drcomputer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import vn.id.quanghuydevfs.drcomputer.dto.order.OrderDto;
+import vn.id.quanghuydevfs.drcomputer.service.OrderService;
+
+@SpringBootTest
+class DrComputerApplicationTests {
+
+    @Autowired
+    private OrderService orderService;
+
+
+
+}
